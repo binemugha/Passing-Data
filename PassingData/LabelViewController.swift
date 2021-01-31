1be6d73fef84094bd5171231985537ee920bfafd
Benjamin Inemugha <binemugha@gmail.com>
2021-01-31 19:41:29 +0100

    first commit

---
+++ PassingData/LabelViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LabelViewController.swift
+//  PassingData
+//
+//  Created by Benjamin Inemugha on 31/01/2021.
+//  Copyright © 2021 Techelope. All rights reserved.
+//
+
+import UIKit
+
+class LabelViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
